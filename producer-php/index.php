3a8fe8c84f398168190c02bbbe3a830f261b564f
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2023-01-06 18:45:56 +0200

    php producer

---
+++ producer-php/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use longlang\phpkafka\Producer\Producer;
+use longlang\phpkafka\Producer\ProducerConfig;
+
+require_once 'vendor/autoload.php';
+
+$config = new ProducerConfig();
+$config->setBootstrapServer('localhost:9092');
+$config->setUpdateBrokers(true);
+$config->setAcks(-1);
+
+$producer = new Producer($config);
+
+$message = microtime(true);
+$key = uniqid('', true);
+
+$producer->send('fancy-topic', $message, $key);
+var_dump('key: '.$key . ' message:' . $message);
+
